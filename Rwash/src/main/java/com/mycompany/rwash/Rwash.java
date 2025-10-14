66324bbf6278af8796d98c4b0380f41e28093a19
Aluno Fatec <aluno@fateclab.net>
2025-10-14 11:15:11 -0300

    primeira alteracao

---
+++ Rwash/src/main/java/com/mycompany/rwash/Rwash.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rwash;
+
+/**
+ *
+ * @author aluno
+ */
+public class Rwash {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
