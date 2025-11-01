dd4985846c8f1de9e4d3fdca152d85cc40ca9e48
Pedro Vicente <pedro2020kk1p@gmail.com>
2025-11-01 20:18:53 -0300

    Banco novo Rwashssql

---
+++ Rwash.sql
@@ -0,0 +1,147 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: r-wash
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `nomeCliente` varchar(45) NOT NULL,
+  `emailCliente` varchar(45) NOT NULL,
+  `cpfCliente` varchar(45) DEFAULT NULL,
+  `senhaCliente` varchar(255) NOT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'teste','teste@email.com',NULL,'123'),(2,'teste','teste@teste',NULL,'123'),(3,'Gustavo','Gustavo@gmail',NULL,'1234'),(4,'PEdro Vicente','Pedroteste@gmail',NULL,'12345'),(5,'ricarod','ricardo@gmail',NULL,'4321'),(6,'kevyn','kevy@gmail',NULL,'8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compra`
+--
+
+DROP TABLE IF EXISTS `compra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compra` (
+  `idCompra` int NOT NULL AUTO_INCREMENT,
+  `idCliente` int NOT NULL,
+  `idMaquina` int NOT NULL,
+  `dataCompra` datetime DEFAULT CURRENT_TIMESTAMP,
+  `statusCompra` enum('pendente','concluida','cancelada') DEFAULT NULL,
+  `enderecoCliente` varchar(45) NOT NULL,
+  `bairroCliente` varchar(45) NOT NULL,
+  `cepCliente` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCompra`),
+  KEY `compra_ibfk_1` (`idCliente`),
+  KEY `compra_ibfk_2` (`idMaquina`),
+  CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`),
+  CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`idMaquina`) REFERENCES `maquina` (`idMaquina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compra`
+--
+
+LOCK TABLES `compra` WRITE;
+/*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maquina`
+--
+
+DROP TABLE IF EXISTS `maquina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maquina` (
+  `idMaquina` int NOT NULL,
+  `modeloMaquina` varchar(45) NOT NULL,
+  `turbidezMaquina` varchar(45) NOT NULL,
+  `capacidadeMaquina` varchar(45) NOT NULL,
+  `Cliente_idCliente` int NOT NULL,
+  PRIMARY KEY (`idMaquina`),
+  KEY `fk_Maquina_Cliente1_idx` (`Cliente_idCliente`),
+  CONSTRAINT `fk_Maquina_Cliente1` FOREIGN KEY (`Cliente_idCliente`) REFERENCES `cliente` (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maquina`
+--
+
+LOCK TABLES `maquina` WRITE;
+/*!40000 ALTER TABLE `maquina` DISABLE KEYS */;
+INSERT INTO `maquina` VALUES (1,'teste','teeste','2',0);
+/*!40000 ALTER TABLE `maquina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensor`
+--
+
+DROP TABLE IF EXISTS `sensor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sensor` (
+  `idSensor` int NOT NULL AUTO_INCREMENT,
+  `dataLeitura` datetime DEFAULT CURRENT_TIMESTAMP,
+  `turbidez` decimal(5,2) DEFAULT NULL,
+  `temperatura` decimal(5,2) DEFAULT NULL,
+  `consumoAgua` decimal(8,2) DEFAULT NULL,
+  `consumoEnergia` decimal(8,2) DEFAULT NULL,
+  `tempoCiclo` int DEFAULT NULL,
+  `idMaquina` int DEFAULT NULL,
+  PRIMARY KEY (`idSensor`),
+  KEY `idMaquina` (`idMaquina`),
+  CONSTRAINT `sensor_ibfk_1` FOREIGN KEY (`idMaquina`) REFERENCES `maquina` (`idMaquina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensor`
+--
+
+LOCK TABLES `sensor` WRITE;
+/*!40000 ALTER TABLE `sensor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sensor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-01 20:18:21
