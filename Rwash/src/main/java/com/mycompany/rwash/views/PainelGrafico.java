b0949fe9a3052edba993db9e279dabf46f39e31d
LatenXT^C <aluno@fateclab.net>
2025-11-07 11:21:38 -0300

    cor do dashboard

--- Rwash/src/main/java/com/mycompany/rwash/views/PainelGrafico.java
+++ Rwash/src/main/java/com/mycompany/rwash/views/PainelGrafico.java
@@ -4,10 +4,13 @@
  */
 package com.mycompany.rwash.views;
 
+import java.awt.Color;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class PainelGrafico extends JPanel {
@@ -21,6 +24,51 @@ public class PainelGrafico extends JPanel {
 
         JFreeChart chart = ChartFactory.createLineChart(
                 titulo, eixoX, eixoY, dataset);
+        
+        chart.setBackgroundPaint(Color.WHITE); // fundo da área do chart
+        CategoryPlot plot = chart.getCategoryPlot();
+
+        // Converter HSV para RGB
+        float hue = 244f / 360f;
+        float saturation = 0.62f;
+        float value = 0.22f;
+        Color roxoFundo = Color.getHSBColor(hue, saturation, value);
+
+        float valuePlot = 0.35f; 
+        Color roxoPlot = Color.getHSBColor(hue, saturation, valuePlot);
+        
+        float saturationLinha = 0.8f;
+        float valueLinha = 0.7f;
+        Color roxoLinha = Color.getHSBColor(hue, saturationLinha, valueLinha);
+        
+       chart.setBackgroundPaint(roxoFundo);  // antes estava Color.BLACK
+        plot.setBackgroundPaint(roxoFundo);          // fundo do gráfico
+        plot.setDomainGridlinePaint(Color.WHITE); // linhas verticais
+        plot.setRangeGridlinePaint(Color.WHITE);  // linhas horizontais
+
+        // Linha do gráfico
+       plot.setDomainGridlinePaint(Color.WHITE);
+plot.setRangeGridlinePaint(Color.WHITE);
+chart.getTitle().setPaint(Color.WHITE);
+plot.getDomainAxis().setLabelPaint(Color.WHITE);
+plot.getDomainAxis().setTickLabelPaint(Color.WHITE);
+plot.getRangeAxis().setLabelPaint(Color.WHITE);
+plot.getRangeAxis().setTickLabelPaint(Color.WHITE);
+
+        // Texto dos eixos
+        plot.getDomainAxis().setLabelPaint(Color.WHITE);
+        plot.getDomainAxis().setTickLabelPaint(Color.WHITE);
+        plot.getRangeAxis().setLabelPaint(Color.WHITE);
+        plot.getRangeAxis().setTickLabelPaint(Color.WHITE);
+
+        // Título
+        chart.getTitle().setPaint(Color.WHITE);
+
+        
+        
+        
+        
+        
 
         ChartPanel painel = new ChartPanel(chart);
         setLayout(new java.awt.BorderLayout());
